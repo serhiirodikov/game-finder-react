22bae2c8e039b07a7947ad9cac0149417727ce0b
serhiirodikov <sergiy1998373@gmail.com>
2020-12-23 02:08:26 +0200

    [ADDED] reducer

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,20 @@
+const initState = {
+  popular: [],
+  newGames: [],
+  upcoming: [],
+}
+
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'FETCH_GAMES':
+      return {
+        ...state,
+        popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
+      }
+    default:
+      return { ...state }
+  }
+}
+export default gamesReducer
