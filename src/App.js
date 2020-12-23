0a02cff66194883c132aca25d78bec2edc954a2c
serhiirodikov <sergiy1998373@gmail.com>
2020-12-23 02:53:37 +0200

    [ADDED] data to our component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { loadGames } from './actions/gamesAction'
+import GlobalStyles from './components/GlobalStyles'
 
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <GlobalStyles />
       <Home />
     </div>
   )
